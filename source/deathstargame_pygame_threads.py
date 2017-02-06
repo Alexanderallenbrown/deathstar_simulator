cc8afc03c4c3a7cd3d0e73942d05cf20d9366850
Alexander Brown <brownaa@lafayette.edu>
2017-02-06 14:47:02 -0500

    added triangle

--- source/deathstargame_pygame_threads.py
+++ source/deathstargame_pygame_threads.py
@@ -263,9 +263,14 @@ class App:
 
         #draw the activator popper
         if self.activator_hit is not 1:
-            pygame.draw.circle(self._display_surf, grey, (self.actx,self.acty), self.actplateradius, 0)
-            pygame.draw.rect(self._display_surf, grey, (self.actx-int(self.actplateradius/2),self.acty-2*self.actplateradius,self.actplateradius,self.actplateradius*6.0), 0)
-            pygame.draw.circle(self._display_surf, grey, (self.actx,self.acty-2*self.actplateradius), int(self.actplateradius/2.), 0)
+            pygame.draw.circle(self._display_surf, white, (self.actx,self.acty), self.actplateradius, 0)
+            pygame.draw.rect(self._display_surf, white, (self.actx-int(self.actplateradius/2),self.acty-2*self.actplateradius,self.actplateradius,self.actplateradius*6.0), 0)
+            pygame.draw.circle(self._display_surf, white, (self.actx,self.acty-2*self.actplateradius), int(self.actplateradius/2.), 0)
+            p1 = (self.width-self.actx,self.height-self.acty)
+            L = 20
+            p2 = (p1[0]-L*sin(pi/6.),p1[1]+L*cos(pi/6.))
+            p3 = (p1[0]+L*sin(pi/6.),p1[1]+L*cos(pi/6.))
+            pygame.draw.polygon(self._display_surf,white,[p1,p2,p3])
         #pygame.draw.rect(self._display_surf, grey, (self.actx-self.actplateradius*.4,self.acty,self.actplateradius,self.actplateradius*3.0), 0)
         #pygame.draw.rect(self._display_surf, grey, (self.actx-self.actplateradius*.4,self.acty-self.actplateradius,self.actplateradius*1.5,self.actplateradius*3.0), 0)
         #pygame.draw.circle(self._display_surf, grey, (self.actx,self.acty-self.actplateradius), int(self.actplateradius/2.0), 0)
